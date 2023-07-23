19eb380e5f02ed60309702a7098699b5b2a309c9
Sadman Shoumik <sadman.iitju@gmail.com>
2023-07-23 21:03:21 +0600

    Initial Commit and Push

---
+++ FlappyPokemon/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace FlappyPokemon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.Magikarp = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pipeTop2 = new System.Windows.Forms.PictureBox();
+            this.pipeBottom2 = new System.Windows.Forms.PictureBox();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Magikarp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.Image = global::FlappyPokemon.Properties.Resources.img_ground;
+            this.ground.Location = new System.Drawing.Point(-5, 609);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(953, 53);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // Magikarp
+            // 
+            this.Magikarp.BackColor = System.Drawing.Color.Transparent;
+            this.Magikarp.Image = global::FlappyPokemon.Properties.Resources.img_Magikarp;
+            this.Magikarp.Location = new System.Drawing.Point(50, 250);
+            this.Magikarp.Margin = new System.Windows.Forms.Padding(0);
+            this.Magikarp.Name = "Magikarp";
+            this.Magikarp.Size = new System.Drawing.Size(69, 70);
+            this.Magikarp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Magikarp.TabIndex = 1;
+            this.Magikarp.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::FlappyPokemon.Properties.Resources.img_pipeTop;
+            this.pipeTop.Location = new System.Drawing.Point(279, -250);
+            this.pipeTop.Margin = new System.Windows.Forms.Padding(0);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(78, 500);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 3;
+            this.pipeTop.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::FlappyPokemon.Properties.Resources.img_pipeBottom;
+            this.pipeBottom.Location = new System.Drawing.Point(279, 450);
+            this.pipeBottom.Margin = new System.Windows.Forms.Padding(0);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(78, 500);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 4;
+            this.pipeBottom.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Turquoise;
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 29);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(110, 35);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // pipeTop2
+            // 
+            this.pipeTop2.Image = global::FlappyPokemon.Properties.Resources.img_pipeTop;
+            this.pipeTop2.Location = new System.Drawing.Point(560, -300);
+            this.pipeTop2.Margin = new System.Windows.Forms.Padding(0);
+            this.pipeTop2.Name = "pipeTop2";
+            this.pipeTop2.Size = new System.Drawing.Size(78, 500);
+            this.pipeTop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop2.TabIndex = 7;
+            this.pipeTop2.TabStop = false;
+            // 
+            // pipeBottom2
+            // 
+            this.pipeBottom2.Image = global::FlappyPokemon.Properties.Resources.img_pipeBottom;
+            this.pipeBottom2.Location = new System.Drawing.Point(560, 400);
+            this.pipeBottom2.Margin = new System.Windows.Forms.Padding(0);
+            this.pipeBottom2.Name = "pipeBottom2";
+            this.pipeBottom2.Size = new System.Drawing.Size(78, 500);
+            this.pipeBottom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom2.TabIndex = 8;
+            this.pipeBottom2.TabStop = false;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.lblGameOver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.Location = new System.Drawing.Point(105, 300);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(0, 42);
+            this.lblGameOver.TabIndex = 9;
+            this.lblGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.BackColor = System.Drawing.Color.PeachPuff;
+            this.lblMax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMax.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(12, 628);
+            this.lblMax.Margin = new System.Windows.Forms.Padding(0);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(59, 26);
+            this.lblMax.TabIndex = 10;
+            this.lblMax.Text = "Max:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(434, 661);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.pipeTop2);
+            this.Controls.Add(this.pipeBottom2);
+            this.Controls.Add(this.Magikarp);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Pokemon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Magikarp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox Magikarp;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.Timer gameTimer;
+        public System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox pipeTop2;
+        private System.Windows.Forms.PictureBox pipeBottom2;
+        private System.Windows.Forms.Label lblGameOver;
+        public System.Windows.Forms.Label lblMax;
+    }
+}
+
---
+++ FlappyPokemon/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyPokemon
+{
+    public partial class Form1 : Form
+    {
+        static int max = 0;
+        int pipeSpeed = 6;
+        int gravity = 8;
+        int score = 0;
+        int i = 0;
+        int flagp1 = 0;
+        int flagp2 = 0;
+        int flip = 1;
+        int hit1 = 0;
+        int hit2 = 0;
+
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            System.Drawing.Drawing2D.GraphicsPath obj = new System.Drawing.Drawing2D.GraphicsPath();
+            obj.AddEllipse(0, 0, Magikarp.Height, Magikarp.Width);
+
+            Region rg = new Region(obj);
+            Magikarp.Region = rg;
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            ShowUpdates();
+
+            if (gravity >= 0 && flip > 0)
+            {
+                Image magi = Magikarp.Image;
+                magi.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                Magikarp.Image = magi;
+                flip = -1;
+            }
+            else if (gravity <= 0 && flip < 0)
+            {
+                Image magi = Magikarp.Image;
+                magi.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                Magikarp.Image = magi;
+                flip = 1;
+            }
+
+
+            Magikarp.Top += gravity;
+
+            pipeBottom.Left -= pipeSpeed;
+            pipeBottom2.Left -= pipeSpeed;
+
+            pipeTop.Left -= pipeSpeed;
+            pipeTop2.Left -= pipeSpeed;
+
+
+            if (pipeTop.Left < Magikarp.Left && flagp1 == 0)
+            {
+                score++;
+                flagp1 = 1;
+            }
+
+            if (pipeTop2.Left < Magikarp.Left && flagp2 == 0)
+            {
+                score++;
+                flagp2 = 1;
+            }
+
+
+            if (pipeTop.Left < -100)
+            {
+                i = rand.Next(230, 480);
+
+                pipeBottom.Left = 560;
+                pipeTop.Left = 560;
+
+                pipeBottom.Top = i;
+                pipeTop.Top = i - 700;
+
+                flagp1 = 0;
+                hit1 = 0;
+            }
+
+            else if(pipeTop2.Left < -100)
+            {
+                i = rand.Next(230, 480);
+
+                pipeBottom2.Left = 560;
+                pipeTop2.Left = 560;
+
+                pipeBottom2.Top = i;
+                pipeTop2.Top = i - 700;
+
+                flagp2 = 0;
+                hit1 = 0;
+            }
+
+
+            if ( (Magikarp.Bounds.IntersectsWith(pipeBottom.Bounds) ||
+                 Magikarp.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                 Magikarp.Bounds.IntersectsWith(pipeBottom2.Bounds) ||
+                 Magikarp.Bounds.IntersectsWith(pipeTop2.Bounds)) && hit1 == 0)
+            {
+                hit1 = 1;
+                endGame();
+            }
+
+            if (Magikarp.Top < -25 || Magikarp.Bottom > 725)
+                endGame();
+                
+
+        }
+
+        private void endGame()
+        {
+            if (score > max)
+                max = score;
+
+            gameTimer.Stop();
+            lblGameOver.Text += "Game Over!!!";
+        }
+
+        private void ShowUpdates()
+        {
+            lblScore.Text = "Score: " + score.ToString();
+            lblMax.Text = "Max: " + max.ToString();
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 8;
+            }
+            
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -8;
+            }
+
+            if(gameTimer.Enabled == false && e.KeyCode == Keys.Enter)
+            {
+                score = 0;
+                Magikarp.Top = 250; Magikarp.Left = 50;
+                pipeBottom.Top = 450; pipeTop.Top = -250;
+                pipeBottom.Left = 279; pipeTop.Left = 279;
+
+                pipeBottom2.Top = 400; pipeTop2.Top = -300;
+                pipeBottom2.Left = 560; pipeTop2.Left = 560;
+
+                gameTimer.Start();
+                lblGameOver.Text = "";
+                hit1 = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
